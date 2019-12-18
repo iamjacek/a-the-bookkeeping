ad7110e23cd09a70f4adfac63edf7faa38f2e72b
iamjacek <iamjacek@icloud.com>
2019-12-18 12:37:01 +0000

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `A Bookkeeping and payrol service`,
+    title: `A - a bookkeeping and payrol service`,
     description: `Payrol and bookkeeping service for self employed and business.`,
     author: `JW`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -21,10 +27,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#E4E4E4`,
+        theme_color: `#004F69`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/a-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
