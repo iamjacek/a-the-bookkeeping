c587e9400aea2615e24de4bfb343d5126a15651a
iamjacek <iamjacek@icloud.com>
2020-02-07 11:27:16 +0000

    file names

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import styled from 'styled-components'
 import TagContainer from '../components/TagContainer'
